19b3ec9ff651283f304de76b4ec49f0504fb3a0a
darwin1501 <darwin.pablo1501@gmail.com>
2024-06-03 14:20:01 +0800

    add error handling

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        NumberSystem.binaryToDecimal();
+        while(true){
+            try{
+                // run the code here.
+                NumberConverter.binaryToDecimal();
+                break;
+            }catch (Exception e){
+                System.out.println("Invalid input try again.");
+            }
+        }
     }
 }
